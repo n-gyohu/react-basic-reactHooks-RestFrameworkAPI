be6bccdabc7b4e40cb7fcf8f527ab23f22d06340
n-gyohu <95629131+n-gyohu@users.noreply.github.com>
2021-12-21 23:48:36 +0900

    API get id data use fetch

--- react-basic/src/components/ApiFetch.js
+++ react-basic/src/components/ApiFetch.js
@@ -4,6 +4,13 @@ import axios from 'axios'
 const ApiFetch = () => {
 
     const [posts,setPosts] = useState([])
+    const [id,setId] = useState(1)
+    const [clicked, setClicked] = useState(false)
+
+    const handlerClicked = () => {
+        setClicked(!clicked)
+    }
+
 
     useEffect(()=>{
         // axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -11,20 +18,25 @@ const ApiFetch = () => {
         //     setPosts(res.data)
         // })
 
-        fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{method: 'GET'})
         .then(res => res.json())
         .then(data => {
             setPosts(data)
         })
-    },[])
+    },[clicked])
 
     return (
         <div>
-            <ul>
+            <input type='text' value={id} onChange={evt=>setId(evt.target.value)}/>
+            <br />
+            <button type='button' onClick={handlerClicked}>Get post</button>
+            <br />
+            {posts.title}
+            {/* <ul>
                 {
                     posts.map(post => <li key={post.id}> {post.title}</li>)
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
